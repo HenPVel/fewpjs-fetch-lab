bc9b6f70a640a75f00fe9f05a39e5de8d526b02e
Henry <hvelas93@gmail.com>
2021-04-09 12:06:17 -0400

    finished lab

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-function fetchBooks() {
+  function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  const books = fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => {
+    console.log(json)
+    renderBooks(json)
+    fetch(characterFind(json).flat()[1032])
+    .then(resp2 => resp2.json())
+    .then(json2 => {
+      console.log(json2.name)
+    })
+  });
 }
 
 function renderBooks(books) {
@@ -10,6 +20,20 @@ function renderBooks(books) {
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
+  const pH2 = document.createElement('p')
+  pH2.innerHTML = findFifthBook(books)
+  main.appendChild(pH2)
+}
+
+function findFifthBook(books) {
+  return `The fifth book is ${books[4].name}.`
+}
+
+function characterFind(books) {
+  const fullCharacterArray =books.map( (book) => {
+    return book.characters
+  } )
+  return fullCharacterArray
 }
 
 document.addEventListener('DOMContentLoaded', function() {
